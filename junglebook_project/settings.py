047ddf4010e8611e5f828caf30a291d30c9e91f0
Mia Sibala <miluchia.sibala@gmail.com>
2021-12-14 21:12:37 -0800

    Add heroku domain to ALLOWED_HOSTS

--- junglebook_project/settings.py
+++ junglebook_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)o1^0jaljizvcs#ej^e7)$&ebsg8td%m*=!mf@qb$of_f6+vm6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['baby-club.herokuapp.com']
 
 
 # Application definition
